1266d1274abf6540f29971d58b3116690ce07575
Peter Dinya <dinyapeti@gmail.com>
2017-05-23 13:42:42 +0200

    git init

---
+++ RailwaysOnline/Models/Journey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RailwaysOnline.Models
+{
+    public class Journey
+    {
+    }
+}
